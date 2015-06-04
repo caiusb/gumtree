94576fc3366af146e3de177aed9a96fe338db5c4
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-06-04 10:21:41 -0700

    Using the new String DiffView

--- client/src/main/java/fr/labri/gumtree/client/StringHtmlDiff.java
+++ client/src/main/java/fr/labri/gumtree/client/StringHtmlDiff.java
@@ -1,9 +1,5 @@
 package fr.labri.gumtree.client;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-
 import org.rendersnake.HtmlCanvas;
 
 import fr.labri.gumtree.client.ui.web.views.DiffView;
@@ -11,19 +7,10 @@ import fr.labri.gumtree.client.ui.web.views.DiffView;
 public class StringHtmlDiff {
 	
 	public String getHtmlOfDiff(String urlFolder, String aContent, String bContent, String aName, String bName) throws Exception {
-		
-		Path file1 = Files.createTempFile("A", ".java");
-		Path file2 = Files.createTempFile("B", ".java");
-		Files.write(file1, aContent.getBytes(), StandardOpenOption.WRITE);
-		Files.write(file2, bContent.getBytes(), StandardOpenOption.WRITE);
-		
-		DiffView diffView = new DiffView(file1.toFile(), file2.toFile(), aName, bName);
+		DiffView diffView = new DiffView(aContent, bContent);
 		diffView.setURLFolder(urlFolder);
 		HtmlCanvas html = new HtmlCanvas();
 		diffView.renderOn(html);
-		
-		file1.toFile().delete();
-		file2.toFile().delete();
 		return html.toHtml();
 	}
 }
